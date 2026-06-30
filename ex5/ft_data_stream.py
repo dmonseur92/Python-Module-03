325e5e2a2f7c8b9aee6b2b35aa891e29f375b3f6
Denis <denismonseur1992@gmail.com>
2026-06-30 18:42:50 +0200

    fds

---
+++ ex5/ft_data_stream.py
@@ -0,0 +1,31 @@
+import random
+from typing import Generator
+
+def gen_event() -> Generator[tuple[str, str], None, None]:
+    players = ("Alice", "Dylan", "Bob", "Charlie")
+    actions = ("run", "eat", "sleep", "move", "swim")
+
+    while True:
+        yield(random.choice(players), random.choice(actions))
+
+def consume_event(events_list: list[tuple[str, str]]) -> Generator[tuple[str, str], None, None]:
+    while len(events_list) > 0:
+        remove = random.randrange(len(events_list))
+        yield events_list.pop(remove)
+
+
+if __name__=="__main__":
+    g = gen_event()
+    for i in range(0, 1000):
+        name, action = next(g)
+        print(f"Event {i}: Player {name} did action {action}")
+
+    events_list = []
+    for _ in range(0, 10):
+        events_list.append(next(g))
+    print(f"Built list of 10 events: {events_list}")
+
+    for event in consume_event(events_list):
+        print(f"Got event from list: {event}")
+        print(f"Remains in list: {events_list}")
+
